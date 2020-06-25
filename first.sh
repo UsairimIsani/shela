b5ea2b54c3f26295bf72e4b4d8757a2e7a10c6c7
Usairim Isani <usairim.isani@outlook.com>
2020-06-25 11:26:17 +0500

    Variables and User Input

--- first.sh
+++ first.sh
@@ -1,2 +1,17 @@
 # ! /bin/bash   
 echo "Hello World"
+#This is a comment and I am chicken.
+echo $BASH
+echo $BASH_VERSION
+echo $BASH_VERSINFO
+echo $COLUMNS
+echo $UID
+echo $PWD
+# printenv
+name="USairim ISani"
+echo This is $name
+echo "Enter Name : "
+read name name1 name2 
+echo "Entered Name : " $name, $name1, $name2
+read -p "USarim" name
+echo "Usairim $name"
