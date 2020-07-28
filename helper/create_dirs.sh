9c5776aa9e3ec11d459731803daba52b41fe5836
Usairim Isani <usairim.isani@outlook.com>
2020-07-28 22:55:57 +0500

    Changed Create Dir Script.

--- helper/create_dirs.sh
+++ helper/create_dirs.sh
@@ -1,5 +1,21 @@
-mkdir /home/$USER/Documents/projects/personal
-mkdir /home/$USER/Documents/projects/work
-mkdir /home/$USER/Documents/University
-mkdir /home/$USER/Documents/Books
-mkdir /home/$USER/Documents/Banks
+personal="/home/$USER/Documents/projects/personal"
+work="/home/$USER/Documents/projects/work"
+opso="/home/$USER/Documents/projects/opso"
+uni="/home/$USER/Documents/University"
+books="/home/$USER/Documents/Books"
+banks="/home/$USER/Documents/Banks"
+dir=($personal $work $opso $uni $books $banks)
+git="git clone"
+for i in ${dir[@]}; do
+    mkdir $i
+    if [[ "$i" == "$personal" ]]; then
+        cd $i
+        $git https://github.com/UsairimIsani/shela.git
+        $git https://github.com/UsairimIsani/rust-playground.git
+        $git https://github.com/UsairimIsani/adm.git
+    fi
+    if [[ "$i" == "$opso" ]]; then
+        cd $i
+        $git https://github.com/rusty-snake/fjp.git
+    fi
+done
