ebb2c940111c8bdbbf87463b07e1625c3b8445d3
Usairim Isani <usairim.isani@outlook.com>
2020-07-28 22:33:08 +0500

    Updated Aliases for Open source projects.

--- helper/create_aliases.sh
+++ helper/create_aliases.sh
@@ -1,13 +1,14 @@
 rm ~/.bash_aliases
 echo $'\n''alias ci="code-insiders ."' >>~/.bash_aliases
 echo $'\n''alias cis="code-insiders "' >>~/.bash_aliases
-echo $'\n''alias shela="code-insiders ~/shela"' >>~/.bash_aliases
+echo $'\n''alias shela="code-insiders ~/Documents/projects/personal/shela"' >>~/.bash_aliases
 echo $'\n''alias projects="cd ~/Documents/projects"' >>~/.bash_aliases
 echo $'\n''alias personal="cd ~/Documents/projects/personal"' >>~/.bash_aliases
 echo $'\n''alias work="cd ~/Documents/projects/work"' >>~/.bash_aliases
+echo $'\n''alias opso="cd ~/Documents/projects/opso"' >>~/.bash_aliases
 echo $'\n''alias uni="cd ~/Documents/University"' >>~/.bash_aliases
 echo $'\n''alias books="cd ~/Documents/Books"' >>~/.bash_aliases
-echo $'\n''alias bank="cd ~/Documents/Bank"' >>~/.bash_aliases
+echo $'\n''alias bank="cd ~/Documents/Banks"' >>~/.bash_aliases
 echo $'\n''alias crs="cd ~/Documents/Courses"' >>~/.bash_aliases
 echo $'\n''alias dl="bash /home/usairim/shela/helper/done-list.sh"' >>~/.bash_aliases
 echo $'\n''alias exs="cd $(exercism workspace)"' >>~/.bash_aliases
