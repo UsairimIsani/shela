7cef6aa1073f04260dcdb53736af60ed7f0c41b5
Usairim Isani <usairim.isani@outlook.com>
2020-08-03 21:01:32 +0500

    Start Stop Services.

---
+++ helper/start_serv.sh
@@ -0,0 +1,5 @@
+sudo service docker start
+sudo service nginx start
+sudo service postgresql start
+sudo service openvpn start
+sudo service --status-all
---
+++ helper/stop_serv.sh
@@ -0,0 +1,5 @@
+sudo service docker stop
+sudo service nginx stop
+sudo service postgresql stop
+sudo service openvpn stop
+sudo service --status-all
