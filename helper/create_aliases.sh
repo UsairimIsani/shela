bf8fffc00de76804d8f5db6c22cf15eb20031055
Usairim Isani <usairim.isani@outlook.com>
2020-08-31 17:58:01 +0500

    Android Studio.

--- helper/create_aliases.sh
+++ helper/create_aliases.sh
@@ -13,3 +13,4 @@ echo $'\n''alias crs="cd ~/Documents/Courses"' >>~/.bash_aliases
 echo $'\n''alias dl="bash /home/usairim/shela/helper/done-list.sh"' >>~/.bash_aliases
 echo $'\n''alias exs="cd $(exercism workspace)"' >>~/.bash_aliases
 echo $'\n''alias adm="code-insiders ~/Documents/projects/personal/adm"' >>~/.bash_aliases
+echo $'\n''alias adio="/usr/local/android-studio/bin/studio.sh"' >>~/.bash_aliases
